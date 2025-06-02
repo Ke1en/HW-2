e35267018d96ca843c517f743c929f5001b16d30
Ke1en <Andpey-dok@mail.ru>
2025-06-02 21:43:22 +0600

    added main implementation

--- src/main/java/Java412/Main.java
+++ src/main/java/Java412/Main.java
@@ -2,5 +2,12 @@ package Java412;
 
 public class Main {
     public static void main(String[] args) {
+        String cardNumber = "1234 5678 test 5435 test 3244";
+
+        if (CreditCardValidator.isValid(cardNumber)) {
+            System.out.println("Valid Card Number");
+        } else {
+            System.out.println("Invalid Card Number");
+        }
     }
 }
